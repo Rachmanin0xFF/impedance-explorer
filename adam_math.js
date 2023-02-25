7e1ec1df45617f75e5a99480a0e3fb432d716c32
Adam Lastowka <adamlastowka@gmail.com>
2023-02-25 02:12:24 -0500

    added star-mesh impedance solver

--- adam_math.js
+++ adam_math.js
@@ -6,6 +6,12 @@ class Cpl {
         this.re = a;
         this.im = b;
     }
+    print_self() {
+        print(this.get_str());
+    }
+    get_str() {
+        return "(" + this.re + " + " + this.im + "j" + ")";
+    }
 }
 function c_one() {
     return new Cpl(1.0, 0.0);
@@ -35,9 +41,11 @@ function cdiv(z, a) {
     return new Cpl(z.re/a, z.im/a);
 }
 function cmult(a, b) {
+    if(a.re === Infinity || b.re === Infinity) return c_infty();
     return new Cpl(a.re*b.re - a.im*b.im, a.re*b.im + a.im*b.re);
 }
 function cdiv(a, b) {
+    if(b.re === Infinity) return c_zero();
     let r2 = 1.0/(b.re*b.re + b.im*b.im);
     return new Cpl(r2*(a.re*b.re+a.im*b.im), r2*(a.im*b.re - a.re*b.im));
 }
@@ -82,4 +90,57 @@ function Z_wire() {
 function Z_open() {
     return c_infty();
 }
-// Matrices (TODO)
+function parallel(Z_a, Z_b) {
+    if(Z_a.re === Infinity) return Z_b;
+    if(Z_b.re === Infinity) return Z_a;
+    return cdiv(cmult(Z_a, Z_b), cadd(Z_a, Z_b));
+}
+function series(Z_a, Z_b) {
+    if(Z_a.re === Infinity || Z_b.re === Infinity) return c_infty();
+    return cadd(Z_a, Z_b);
+}
+
+// Matrices
+
+function star_mesh_reduce(matrix, index) {
+    let Z_inv_total = c_zero();
+    for(let i = 0; i < matrix.length; i++) {
+        if(i != index) {
+            Z_inv_total = cadd(Z_inv_total, cdiv(c_one(), matrix[i][index]));
+        }
+    }
+    let matrix_out = [...Array(matrix.length)].map(e => Array(matrix.length).fill(Z_open()));
+    for(let i = 0; i < matrix_out.length; i++) {
+        if(i != index) {
+            for(let j = 0; j < i; j++) {
+                if(j != index) {
+                    matrix_out[i][j] = parallel(cmult(cmult(matrix[j][index], matrix[i][index]), Z_inv_total), matrix[i][j]);
+                    matrix_out[j][i] = matrix_out[i][j];
+                }
+            }
+            matrix_out[i][i] = c_zero();
+        }
+    }
+    return matrix_out;
+}
+
+function get_equivalent_impedance(matrix, node_A, node_B) {
+    for(let k = 0; k < matrix.length; k++) {
+        if(k != node_A && k != node_B)
+            matrix = star_mesh_reduce(matrix, k);
+    }
+    return matrix[node_A][node_B];
+}
+
+function print_matrix(m) {
+    print("MATRIX:")
+    print("======================");
+    for(let i = 0; i < m.length; i++) {
+        let s = "";
+        for(let j = 0; j < m[i].length; j++) {
+            s += m[i][j].get_str() + " ";
+        }
+        print(s);
+    }
+    print("======================");
+}
